4e462d217653115e73ea1b4894488c7acd0d1ee4
Arnaud Limbourg <arnaud@limbourg.com>
2013-03-06 15:24:40 +0100

    retab

--- plugins/ads.js
+++ plugins/ads.js
@@ -16,6 +16,8 @@ var impl = {
   complete: false,
   ad_is_loaded: false,
   running: false,
+  aborted: false,
+  timeout: 1000,
 
   do_test: function() {
     BOOMR.debug('ads: called do_test');
@@ -28,28 +30,46 @@ var impl = {
 
   load_img: function(src) {
     BOOMR.debug('ads: called load_img');
-    var img = new Image(), 
+    var img = new Image(),
+        timer,
         that = this;
 
     img.onload = function() {
+      img.onload = img.onerror = null;
+      img = null;
+      clearTimeout(timer);
       BOOMR.debug('ads are loaded it seems');
       that.ad_is_loaded = true;
       that.running = false;
       that.finish();
+      that = null;
     };
 
     img.onerror= function() {
+      img.onload = img.onerror = null;
+      img = null;
+      clearTimeout(timer);
+      if (this.aborted) {
+        return false;
+      }
       BOOMR.debug('adblocking in action');
       that.ad_is_loaded = false;
       that.running = false;
       that.finish();
+      that = null;
     };
+
+    timer = setTimeout(function() {
+      return false;
+    }, this.timeout);
 
     img.src = this.ads_src;
   },
 
   finish: function() {
-    BOOMR.addVar("ads", this.ad_is_loaded ? 1 : 0);
+    if (!this.aborted) {
+      BOOMR.addVar("ads", this.ad_is_loaded ? 1 : 0);
+    }
     this.complete = true;
     BOOMR.sendBeacon();
     this.running = false;
@@ -57,29 +77,43 @@ var impl = {
 };
 
 BOOMR.plugins.ads = {
-	init: function(config) {
-		var properties = ["ads_src"];
+  init: function(config) {
+    var properties = ["ads_src"];
 
-		BOOMR.utils.pluginConfig(impl, config, "ads", properties);
+    BOOMR.utils.pluginConfig(impl, config, "ads", properties);
 
     BOOMR.subscribe("page_ready", this.run, null, impl);
 
-		return this;
-	},
+    return this;
+  },
 
   run: function () {
     if (impl.running || impl.complete) {
       return this;
     }
 
+    impl.running = true;
+    impl.aborted = false;
+
+    setTimeout(this.abort, impl.timeout);
+
     impl.do_test();
 
     return this;
   },
 
-	is_complete: function() {
+  abort: function() {
+    BOOMR.debug('abort called');
+    impl.aborted = true;
+    if (impl.running) {
+      impl.finish();
+    }
+    return this;
+  },
+
+  is_complete: function() {
     return impl.complete;
-	}
+  }
 };
 
 }(window));
